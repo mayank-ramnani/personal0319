b4c5f290b8935297f34c3d56c5abe6a0f7d5779c
mayank-ramnani <mayankr99@gmail.com>
2019-04-20 02:10:16 +0530

    media query progress 1

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,8 @@
 // $("#back-to-top")
+// add class menuVisible to body when function triggered.
+// remove class from body when anything else clicked or function triggered again.
+// $("")
+function toggleFunction() {
+  $(this).toggleClass("active"); //check this
+  $(".containerMenu").toggleClass("open");
+}
