3d5474ba40d4ad068b64b9567256c2aa06867e01
mayank-ramnani <mayankr99@gmail.com>
2019-04-21 20:19:03 +0530

    bunch of corrections

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,7 +2,16 @@
 // add class menuVisible to body when function triggered.
 // remove class from body when anything else clicked or function triggered again.
 // $("")
-function toggleFunction() {
-  $(this).toggleClass("active"); //check this
-  $(".containerMenu").toggleClass("open");
-}
+
+// function toggleFunction() {
+//   $(this).toggleClass("active"); //check this
+//   $(".hamburger").toggleClass("is-active");
+//   $(".containerMenu").slideDown("slow");
+//   // $(".containerMenu").toggleClass("open");
+// }
+
+$(function() {
+  $(".hamburger").click(function(event) {
+    $(".containerMenu").slideToggle({ duration: 600 });
+  });
+});
