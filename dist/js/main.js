46dc7256c2b3400b29ac5dd27a792261b6a51712
mayank-ramnani <mayankr99@gmail.com>
2019-04-22 00:05:12 +0530

    improvements to burger menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,15 +3,16 @@
 // remove class from body when anything else clicked or function triggered again.
 // $("")
 
-// function toggleFunction() {
-//   $(this).toggleClass("active"); //check this
-//   $(".hamburger").toggleClass("is-active");
-//   $(".containerMenu").slideDown("slow");
-//   // $(".containerMenu").toggleClass("open");
-// }
-
 $(function() {
   $(".hamburger").click(function(event) {
     $(".containerMenu").slideToggle({ duration: 600 });
+    $(".navbar").addClass("shrink");
+    $(".navbar").removeClass("unshrink");
+  });
+});
+
+$(function() {
+  $(".containerMenu").click(function(e) {
+    $(".containerMenu").slideUp({ duration: 300 });
   });
 });
